2d3aa17467836a3a402dd114a8dbc2b185bcd471
jlucasaf <joaolucas19982@gmail.com>
2023-10-21 19:16:42 -0300

    feat:✨Feito alterações para o formato do trabalho pedido

---
+++ trabalho1/dominios.h
@@ -0,0 +1,10 @@
+#ifndef DOMINIOS_H_INCLUDED
+#define DOMINIOS_H_INCLUDED
+
+#include <stdexcept>
+
+
+
+
+
+#endif // DOMINIOS_H_INCLUDED
---
+++ trabalho1/testes.h
@@ -0,0 +1,9 @@
+#ifndef ENTIDADES_H_INCLUDED
+#define ENTIDADES_H_INCLUDED
+
+#include "dominios.h"
+#include <string>
+
+
+
+#endif // ENTIDADES_H_INCLUDED
