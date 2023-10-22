63cf4e631fa58e2b13145d3b3d7c9a6a9fc4c32e
jlucasaf <joaolucas19982@gmail.com>
2023-10-22 01:10:40 -0300

    feat:✨alguns testes implementados
    foi implementado os testes de codigo e de limite

--- trabalho1/testes.cpp
+++ trabalho1/testes.cpp
@@ -1,1 +1,103 @@
-#include "entidades.h"
+#include "testes.h"
+
+const std::string TUCodigo::CODIGO_VALIDO = "AA12";
+const std::string TUCodigo::CODIGO_INVALIDO = "a1B1";
+
+// TUCodigo
+// implementacao: 190015187
+void TUCodigo::setUp(){
+    codigo = new Codigo();
+    estado = SUCESSO;
+
+}
+void TUCodigo::tearDown(){
+    delete codigo;
+}
+void TUCodigo::testarCenarioFalha()
+{
+
+    try{
+        codigo->setValor(CODIGO_INVALIDO);
+        estado = FALHA;
+    }
+    catch(invalid_argument &excecao)
+    {
+        if(codigo->getValor() == CODIGO_INVALIDO)
+            estado = FALHA;
+    }
+}
+void TUCodigo::testarCenarioSucesso()
+{
+
+    try
+    {
+        codigo->setValor(CODIGO_VALIDO);
+        if(codigo->getValor() != CODIGO_VALIDO)
+            estado = FALHA;
+    }
+    catch(invalid_argument &excecao)
+    {
+        estado = FALHA;
+    }
+}
+int TUCodigo::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
+
+
+
+// TULimite
+// implementacao: 190015187
+void TULimite::setUp(){
+    limite = new Limite();
+    estado = SUCESSO;
+
+}
+void TULimite::tearDown(){
+    delete limite;
+}
+void TULimite::testarCenarioFalha()
+{
+
+    try{
+        limite->setValor(LIMITE_INVALIDO);
+        estado = FALHA;
+    }
+    catch(invalid_argument &excecao)
+    {
+        if(limite->getValor() == LIMITE_INVALIDO)
+            estado = FALHA;
+    }
+}
+void TULimite::testarCenarioSucesso()
+{
+
+    try
+    {
+        limite->setValor(LIMITE_VALIDO);
+        if(limite->getValor() != LIMITE_VALIDO)
+            estado = FALHA;
+    }
+    catch(invalid_argument &excecao)
+    {
+        estado = FALHA;
+    }
+}
+int TULimite::run(){
+    setUp();
+    testarCenarioSucesso();
+    testarCenarioFalha();
+    tearDown();
+    return estado;
+}
+
+
+
+
+
+
+
--- trabalho1/testes.h
+++ trabalho1/testes.h
@@ -1,9 +1,71 @@
-#ifndef ENTIDADES_H_INCLUDED
-#define ENTIDADES_H_INCLUDED
+#ifndef TESTES_H_INCLUDED
+#define TESTES_H_INCLUDED
 
 #include "dominios.h"
 #include <string>
 
+using namespace std;
+// Dominios
+
+//implementacao: 190015187
+// revisao:
+class TUCodigo{
+private:
+    const static string CODIGO_VALIDO;
+    const static string CODIGO_INVALIDO;
+    Codigo *codigo;
+    int estado;
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+public:
+    const static int SUCESSO = 0;
+    const static int FALHA = -1;
+    int run();
+};
 
 
-#endif // ENTIDADES_H_INCLUDED
+
+class TUColuna{
+};
+
+class TUEmail{
+};
+
+class TULimite{
+private:
+    const static int LIMITE_INVALIDO = 25;
+    const static int LIMITE_VALIDO = 10;
+    Limite *limite;
+    int estado;
+    void setUp();
+    void tearDown();
+    void testarCenarioSucesso();
+    void testarCenarioFalha();
+public:
+    const static int SUCESSO = 0;
+    const static int FALHA = -1;
+    int run();
+};
+
+class TUSenha{
+};
+
+class TUTexto{
+};
+
+
+// Entidades
+class TUConta{
+};
+
+class TUQuadro{
+};
+
+class TUCartao{
+};
+
+
+
+#endif // TESTES_H_INCLUDED
